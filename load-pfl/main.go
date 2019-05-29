d038232aa06c72557c4d8bc65aa4fb54c6b694e3
Paul Taylor <ptaylor@ricoh.com.au>
2019-05-30 07:18:29 +1000

    remove comments

--- load-pfl/main.go
+++ load-pfl/main.go
@@ -11,15 +11,9 @@ import (
 )
 
 func main() {
-	//	in := `first_name	last_name	username
-	//"Rob"	"Pike"	rob
-	//Ken	Thompson	ken
-	//"Robert"	"Griesemer"	"gri"
-	//`
 	fileName := path.Join("..", "data", "SR_HFL_PFL_Delta_CSV_20190409", "SR_SAT_ADDR_HFL_PFL_Delta_20190409_201407.csv")
 	fmt.Println(fileName)
 
-	//r := csv.NewReader(strings.NewReader(in))
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
